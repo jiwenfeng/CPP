3bd3b82d45ddab4c4aa755f1cdb10e5dbf1c576e
wenfengji <490257094@qq.com>
2016-05-15 23:05:24 +0800

    ***

---
+++ leetcode/3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+/*
+ * Given a string, find the length of the longest substring without repeating characters
+ *
+ * Given "abcabcbb" ==> "abc", length is 3
+ * Given "bbbbbbbb" ==> "b", length is 1
+ *
+ * */
+
+class Solution
+{
+public:
+	int lenghtOfLongestSubstring(string s)
+	{
+		int sz = s.size();
+		if(0 == sz)
+		{
+			return 0;
+		}
+		int j, p, len = 1;
+		for(int i = 1; i < sz; ++i)
+		{
+			int j = p;
+			for(; j < i; ++j)
+			{
+				if(s[i] == s[j])
+				{
+					p = j + 1;
+					break;
+				}
+			}
+			len = max(len, i - p + 1);
+		}
+		return len;
+	}
+};
