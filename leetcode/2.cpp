8b640c7537f48e82a2570d7e8d91076d99900561
wenfengji <490257094@qq.com>
2016-05-15 18:01:34 +0800

    ****

--- leetcode/2.cpp
+++ leetcode/2.cpp
@@ -19,59 +19,43 @@ struct ListNode
 
 class Solution
 {
-	public:
-		ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-			ListNode *head = NULL, *i1 = NULL, *i2 = NULL;
-			ListNode **i = &head;
-			int flag = 0;
-			for(i1 = l1, i2 = l2; i1 != NULL && i2 != NULL; i1 = i1->next, i2 = i2->next)
-			{
-				int n = flag + i1->val + i2->val;
-				flag = n / 10;
-				ListNode *node = new ListNode(n % 10);
-				*i = node;
-				i = &node->next;
-			}
-			for(; i1 != NULL; i1 = i1->next)
-			{
-				int n = flag + i1->val;
-				flag = n / 10;
-				ListNode *node = new ListNode(n % 10);
-				*i = node;
-				i = &node->next;
-			}
-			for(; i2 != NULL; i2 = i2->next)
-			{
-				int n = flag + i2->val;
-				flag = n / 10;
-				ListNode *node = new ListNode(n % 10);
-				*i = node;
-				i = &node->next;
-			}
-			if(flag != 0)
-			{
-				ListNode *node = new ListNode(flag);
-				*i = node;
-				i = &node->next;
-
-			}
-			return head;
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+	{
+		ListNode *head = NULL, *i1 = NULL, *i2 = NULL;
+		ListNode **i = &head;
+		int flag = 0;
+		for(i1 = l1, i2 = l2; i1 != NULL && i2 != NULL; i1 = i1->next, i2 = i2->next)
+		{
+			int n = flag + i1->val + i2->val;
+			flag = n / 10;
+			ListNode *node = new ListNode(n % 10);
+			*i = node;
+			i = &node->next;
 		}
+		for(; i1 != NULL; i1 = i1->next)
+		{
+			int n = flag + i1->val;
+			flag = n / 10;
+			ListNode *node = new ListNode(n % 10);
+			*i = node;
+			i = &node->next;
+		}
+		for(; i2 != NULL; i2 = i2->next)
+		{
+			int n = flag + i2->val;
+			flag = n / 10;
+			ListNode *node = new ListNode(n % 10);
+			*i = node;
+			i = &node->next;
+		}
+		if(flag != 0)
+		{
+			ListNode *node = new ListNode(flag);
+			*i = node;
+			i = &node->next;
+
+		}
+		return head;
+	}
 };
-
-int main()
-{
-	ListNode *l1 = new ListNode(0);
-
-	ListNode *l2 = new ListNode(3);
-	ListNode *l3 = new ListNode(7);
-	l2->next = l3;
-
-	Solution s;
-
-	ListNode *head = s.addTwoNumbers(l1, l2);
-
-	cout<<head<<endl;
-
-	return 0;
-}
