524b1387bbeadbddfc4bea5389b957efb55e3eae
wenfengji <490257094@qq.com>
2016-05-16 02:08:51 +0800

    ***

---
+++ leetcode/4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+	double findMedianSortedArrays(vector<int> &n1, vector<int> &n2)
+	{
+		vector<int> v;
+		copy(n1.begin(), n1.end(), back_inserter(v));
+		copy(n2.begin(), n2.end(), back_inserter(v));
+		sort(v.begin(), v.end());
+		size_t sz = v.size();
+		if(sz % 2 == 0)
+		{
+			return double((v[sz / 2] + v[sz / 2 - 1]) / 2.0);
+		}
+		else
+		{
+			return double(v[sz / 2]);
+		}
+	}
+};
+
+int main()
+{
+	Solution s;
+	vector<int> v1 = {}, v2 = {2, 3};
+	cout<<s.findMedianSortedArrays(v1, v2)<<endl;
+	return 0;
+}
