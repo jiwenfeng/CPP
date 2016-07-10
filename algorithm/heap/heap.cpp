fe518ed076269525e821e74a8ef32711c9081cf1
wenfengji <490257094@qq.com>
2016-07-11 02:23:39 +0800

    modify heap to fix stl

--- algorithm/heap/heap.cpp
+++ algorithm/heap/heap.cpp
@@ -1,75 +1,99 @@
 #include "heap.h"
-#include <stdlib.h>
-#include <string.h>
+#include <bits/stl_construct.h>
+#include <bits/stl_iterator.h>
+#include <iostream>
 
-using namespace std;
+#ifdef __TEST_STRING
+#include <string>
+#endif
 
 #define FATHER(n) (((n) - 1) / 2)
 #define LCHILD(n) (2 * (n) + 1)
 #define RCHILD(n) (2 * (n) + 2)
 
-template<class T>
-MaxHeap<T>::MaxHeap(T arr[], int n) : _cap(n), _n(0), _array(NULL)
+template<class T, class Alloc>
+MaxHeap<T, Alloc>::MaxHeap(T arr[], int n) : _cap(n), _n(0), _array(NULL)
 {
-	_array = new T[_cap];
+	_array = _allocator.allocate(n);
 	for(int i = 0; i < n; ++i)
 	{
 		push(arr[i]);
 	}
 }
 
-template<class T>
-MaxHeap<T>::MaxHeap() : _cap(0), _n(0), _array(NULL)
+template<class T, class Alloc>
+MaxHeap<T, Alloc>::MaxHeap() : _cap(0), _n(0), _array(NULL)
 {}
 
-template<class T>
+template<class T, class Alloc>
+MaxHeap<T, Alloc>::MaxHeap(const MaxHeap<T, Alloc> &rhs)
+{
+	this->operator=(rhs);
+}
+
+template<class T, class Alloc>
+MaxHeap<T, Alloc> &
+MaxHeap<T, Alloc>::operator=(const MaxHeap<T, Alloc> &rhs)
+{
+	if(this == &rhs)
+	{
+		return *this;
+	}
+	for(int i = 0; i < rhs._n; ++i)
+	{
+		push(rhs._array[i]);
+	}
+	return *this;
+}
+
+template<class T, class Alloc>
 void
-MaxHeap<T>::expand_heap()
+MaxHeap<T, Alloc>::expand_heap()
 {
 	int cap = _cap < 8 ? 8 : _cap * 2;
-	T *new_array = new T[cap];
-	memcpy(new_array, _array, _n * sizeof(T));
+	T *new_array = _allocator.allocate(cap);
+	for(int i = 0; i < _n; ++i)
+	{
+		_allocator.construct(&new_array[i], _array[i]);
+	}
 	_cap = cap;
-	delete[] _array;
 	_array = new_array;
 }
 
-template<class T>
+template<class T, class Alloc>
 void
-MaxHeap<T>::push(const T &value)
+MaxHeap<T, Alloc>::push(const T &value)
 {
-	if(_n >= _cap - 1)
+	if(_n == _cap)
 	{
 		expand_heap();
 	}
 	heap_shift_up(_n++, value);
 }
 
-template<class T>
+template<class T, class Alloc>
 T
-MaxHeap<T>::pop()
+MaxHeap<T, Alloc>::pop()
 {
-	if(_n == 0)
+	if(0 == _n)
 	{
 		return T();
 	}
-	heap_shift_down();
+	T value = _array[0];
+	heap_shift_down(0, _array[--_n]);
+	return value;
 }
 
-template<class T>
-MaxHeap<T>::~MaxHeap()
+template<class T, class Alloc>
+MaxHeap<T, Alloc>::~MaxHeap()
 {
-	for(int i = 0; i < _n; ++i)
-	{
-		_array[i].T::~T();
-	}
-	delete [] _array;
-	_array = NULL;
+	std::_Destroy(begin(), end(), get_allocator());
+	_allocator.deallocate(_array, _cap);
 }
 
-template<class T>
+template<class T, class Alloc>
 void
-MaxHeap<T>::heap_shift_up(int idx, const T &value)
+MaxHeap<T, Alloc>::heap_shift_up(int idx, const T &value)
 {
 	int pos = FATHER(idx);
 	while(idx)
@@ -82,29 +106,78 @@ MaxHeap<T>::heap_shift_up(int idx, const T &value)
 		idx = pos;
 		pos = FATHER(pos);
 	}
-	_array[idx] = value;
+	_allocator.construct(&_array[idx], value);
 }
 
-template<class T>
+template<class T, class Alloc>
 void 
-MaxHeap<T>::heap_shift_down()
-{}
-
-template<class T>
-void
-MaxHeap<T>::debug()
+MaxHeap<T, Alloc>::heap_shift_down(int pos, const T &value)
 {
-	for(int i = 0; i < _n; ++i)
+	int max_child = RCHILD(pos);
+	while(max_child <= _n)
 	{
-		cout<<_array[i]<<endl;
+		max_child -= max_child == _n || _array[max_child] < _array[max_child - 1];
+		if(_array[max_child] < value)
+		{
+			break;
+		}
+		_array[pos] = _array[max_child];
+		pos = max_child;
+		max_child = RCHILD(pos);
+	}
+	_array[pos] = value;
+}
+
+template<class T, class Alloc>
+void
+MaxHeap<T, Alloc>::debug()
+{
+	std::cout<<"---------------------"<<std::endl;
+	iterator i;
+	for(i = begin(); i != end(); ++i)
+	{
+		std::cout<<(*i)<<std::endl;
 	}
 }
 
+#if __cplusplus >= 201103L
+template<class T, class Alloc>
+MaxHeap<T, Alloc>::MaxHeap(MaxHeap &&rhs)
+{
+	std::_Destroy(begin(), end(), get_allocator());
+	_allocator.deallocate(_array, _cap);
+	this->_n = rhs._n;
+	this->_cap = rhs._cap;
+	this->_array = rhs._array;
+	rhs._array = NULL;
+	rhs._n = rhs._cap = 0;
+}
+#endif
+
+
 int main()
 {
+#ifdef __TEST_INT__
 	int arr[] = {12, 32, 39, 80, 79, 73, 90, 10, 54, 87};
 #define N (sizeof(arr) / sizeof(arr[0]))
 	MaxHeap<int> iHeap(arr, N);
 	iHeap.debug();
+
+	iHeap.pop();
+	iHeap.debug();
+
+	iHeap.pop();
+	iHeap.debug();
+
+	MaxHeap<int>::iterator itr;
+
+#endif
+
+#ifdef __TEST_STRING__
+	std::string arr[] = {"ff", "aa", "ss", "bb", "cc", "dd", "hh", "ii", "kk"};
+#define N (sizeof(arr) / sizeof(arr[0]))
+	MaxHeap<std::string> sHeap(arr, N);
+	sHeap.debug();
+#endif
 	return 0;
 }
--- algorithm/heap/heap.h
+++ algorithm/heap/heap.h
@@ -1,27 +1,119 @@
 #ifndef __HEAP_H__
 #define __HEAP_H__
-#include <iostream>
+
+#include <iterator>
 
 template<class T>
+class HeapIterator
+{
+
+public:
+	typedef typename std::iterator_traits<T>::value_type value_type;
+	typedef typename std::iterator_traits<T>::pointer pointer;
+	typedef typename std::iterator_traits<T>::reference reference;
+	typedef typename std::iterator_traits<T>::iterator_category iterator_category;
+	typedef typename std::iterator_traits<T>::difference_type difference_type;
+
+public:
+	HeapIterator(const T &value = T()) : _value(value)
+	{
+	}
+
+	~HeapIterator() {}
+
+	value_type operator++(int)
+	{
+		T v = *_value;
+		_value++;
+		return v;
+	}
+
+	reference operator++()
+	{
+		++_value;
+		return *_value;
+	}
+
+	bool operator != (const HeapIterator<T> &rhs)
+	{
+		return _value != rhs._value;
+	}
+
+	bool operator == (const HeapIterator<T> &rhs)
+	{
+		 return _value == rhs._value;
+	}
+
+	reference operator*()
+	{
+		return *_value;
+	}
+
+private:
+	T		_value;
+};
+
+template<class T, class Alloc = std::allocator<T> >
 class MaxHeap
 {
-private:
-	int _n;
-	int _cap;
-	T *_array;
-private:
-	void heap_shift_up(int pos, const T &value);
-	void heap_shift_down();
-	void expand_heap();
+public:
+
+	typedef T				value_type;
+	typedef T*				pointer;
+	typedef T&				reference;
+	typedef const T*		const_pointer;
+	typedef const T&		const_reference;
+
+	typedef HeapIterator<pointer> iterator;
+	typedef HeapIterator<const_pointer> const_iterator;
+
 public:
 	MaxHeap(T arr[], int n);
 	MaxHeap();
-	void push(const T &value);
-	T pop();
+	MaxHeap(const MaxHeap<T, Alloc> &rhs);
+	MaxHeap &operator=(const MaxHeap<T, Alloc> &rhs);
+#if __cplusplus >= 201103L
+	MaxHeap(MaxHeap &&rhs);
+#endif
 	~MaxHeap();
 
 public:
+	void push(const T &value);
+	T pop();
 	void debug();
+
+public:
+	iterator begin()
+	{
+		return iterator(_array);
+	}
+
+	iterator end()
+	{
+		return iterator(_array + _n);
+	}
+
+	const_iterator begin() const
+	{
+		return const_iterator(_array);
+	}
+
+	const_iterator end() const
+	{
+		return const_iterator(_array + _n);
+	}
+
+private:
+	void heap_shift_up(int pos, const T &value);
+	void heap_shift_down(int pos, const T &value);
+	void expand_heap();
+	Alloc &get_allocator() { return _allocator; }
+
+private:
+	int _n;
+	int _cap;
+	pointer _array;
+	Alloc _allocator;
 };
 
 #endif
